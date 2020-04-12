db3d6fbc382d819fa3343fc0c057f9a616ae83ce
Burak Cabadan <55558309+burakcbdn@users.noreply.github.com>
2020-04-12 15:19:39 +0300

    Interfaces added

---
+++ src/BackpackInterface.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public interface BackpackInterface<T> {
+
+    //GETTERS
+    public double getCapacity();
+
+    public int getItemCount();
+
+    public double getCurrentWeight();
+
+    public ArrayList<T> getItems();
+
+
+    //SETTERS
+    public void setCurrentWeight(double currentWeight);
+
+    public void setDifficultyLevel(int difficultyLevel);
+
+    public void setItemCount(int itemCount);
+
+    public void setItems(ArrayList<T> items);
+
+    public void setCapacity();
+
+
+    //FUNCTIONS
+    public boolean addItem(T item);
+
+    public boolean isFull();
+
+}
---
+++ src/BoxInterface.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public interface BoxInterface<T> {
+
+    //GETTERS
+    public double getTotalWeight();
+
+    public int getItemCount();
+
+    public ArrayList<T> getItems();
+
+    public String getItemType();
+
+    //SETTERS
+    public void setItems(ArrayList<T> items);
+
+    public void setItemCount(int itemCount);
+
+    public void setItemType(String itemType);
+
+    public void setTotalWeight(double totalWeight);
+
+    //FUNCTIONS
+    public void addItem(T item);
+
+    public boolean removeItem(T itemForRemove);
+
+    //CHECKERS
+    public boolean isBoxEmpty();
+
+    public boolean contains(T item);
+}
